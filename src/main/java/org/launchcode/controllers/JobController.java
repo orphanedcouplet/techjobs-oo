108b2fb485df0e78ceaf643d501d700c741ab43a
orphanedcouplet <oliverdayers@gmail.com>
2017-12-08 11:40:59 -0600

    working on TODO 6 (figured out how to use JobFieldData, next step: figure out why I can't see the fields of the new job other than name)

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,6 +1,7 @@
 package org.launchcode.controllers;
 
-import org.launchcode.models.Job;
+import org.launchcode.models.*;
+import org.launchcode.models.data.JobFieldData;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.text.MessageFormat;
 
 /**
  * Created by LaunchCode
@@ -45,7 +47,23 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        JobFieldData<Employer> employerJobFieldData = new JobFieldData<>();
+        JobFieldData<Location> locationJobFieldData = new JobFieldData<>();
+        JobFieldData<PositionType> positionTypeJobFieldData = new JobFieldData<>();
+        JobFieldData<CoreCompetency> coreCompetencyJobFieldData = new JobFieldData<>();
+
+        String name = jobForm.getName();
+        Employer employer = employerJobFieldData.findById(jobForm.getEmployerId());
+        Location location = locationJobFieldData.findById(jobForm.getLocationId());
+        PositionType positionType = positionTypeJobFieldData.findById(jobForm.getPositionTypeId());
+        CoreCompetency coreCompetency = coreCompetencyJobFieldData.findById(jobForm.getCoreCompetencyId());
+
+        Job job = new Job(name, employer, location, positionType, coreCompetency);
+
+        // TODO fix bug: this is adding the job, but only the name field works - why??
+        JobData.getInstance().add(job);
+
+        return MessageFormat.format("redirect:?id={0}", job.getId());
 
     }
 }
